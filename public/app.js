65c60d733db98515379849dc5122f00fbdec9a58
Natasha Neeck <natashaneeck@gmail.com>
2026-01-03 12:03:38 -0500

    added helpers and conversation class object

--- public/app.js
+++ public/app.js
@@ -1,3 +1,74 @@
+//I should probably have some documentation huh
+// fix single letter variable names
+
+function randomizeArray(arr) {
+    let shuffled = [...arr];
+
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; 
+    }
+    
+    return shuffled;
+}
+
+function assignPairs(arr) {
+    let shuffled = randomizeArray(arr);
+    let map = new Map(); 
+    
+    for (let i = 0; i < arr.length; i++) {
+        map.set(arr[i], shuffled[(i + 1) % arr.length]);
+    }
+    
+    return map;
+}
+
+class Conversation {
+    #chat;
+    #ended;
+    #chatters;
+
+    constructor(chatOwner, chatParticipant) {
+        this.#chat = new Array();
+        this.#ended = false;
+        this.#chatters = [chatOwner, chatParticipant];
+    }
+
+    addLine(line) {
+        if (!this.#ended) {
+            this.#chat.push(line);
+        } 
+    }
+
+    addExactly() {
+        this.addLine("Exactly!");
+        this.addLine("Exactly!");
+        this.#ended = true;
+    }
+
+    removeExactly() {
+        if (this.#chat[this.#chat.length] == "Exactly!" 
+            && this.#chat[this.#chat.length - 1] == "Exactly!") {
+                this.#chat.pop();
+                this.#chat.pop();
+                this.#ended = false;
+            }
+    }
+
+    viewChat() {
+        return [...this.#chat];
+    }
+
+    isEnded() {
+        return this.#ended;
+    }
+
+    getSpeaker(msgIdx) {
+        return this.#chatters[msgIdx % 2]; //Assumes chat owner starts conversation
+    }
+}
+
+
 (function () {
     let playerId;
     let playerRef; // Corresponding firebase node to this user so we can update it
